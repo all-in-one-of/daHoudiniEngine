7e5a155f838e7c4286ca3227d44da6e77921c699
Darren Lee <darren.lee@uts.edu.au>
2015-04-28 11:03:27 +1000

    houdini engine geometry through cyclops
    HoudiniGeometry class extending cyclops::ModelGeometry
    
    example reading normals and positions of point attributes

--- examples/daOsgHEngine/daOsgHEngine.cpp
+++ examples/daOsgHEngine/daOsgHEngine.cpp
@@ -28,15 +28,22 @@
  *		A slightly more advanced cyclops application. Loads a mesh and lets the user manipulate it.
  *		This demo introduces the use of the SceneEditor classes and the menu system.
  **************************************************************************************************/
-#include <omega.h>
 #include <cyclops/cyclops.h>
-#include <omegaToolkit.h>
 #include "HAPI_CPP.h"
+
+#define OMEGA_NO_GL_HEADERS
+#include <omega.h>
+#include <omegaOsg/omegaOsg.h>
+#include <omegaToolkit.h>
 
 #include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <vector>
+
+// TODO: look at cyclops/ModelGeometry.cpp, use something like this class to make
+// houdini engine objects, and then add some parameter manipulation in a menu
+// also, see if display lists help or not..
 
 using namespace std;
 using namespace omega;
@@ -60,6 +67,61 @@ using namespace omegaToolkit::ui;
 	exit(1); \
     }
 
+class HoudiniGeometry : public ModelGeometry
+{
+public:
+	static HoudiniGeometry* create(const String& name)
+	{
+		return new HoudiniGeometry(name);
+	}
+
+	int addNormal(const Vector3f& v);
+	void setNormal(int index, const Vector3f& v);
+	Vector3f getNormal(int index);
+
+public:
+	HoudiniGeometry(const String& name): ModelGeometry(name) {
+	}
+private:
+	Ref<osg::Vec3Array> myNormals;
+
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+int HoudiniGeometry::addNormal(const Vector3f& v)
+{
+	if(myNormals == NULL) {
+		myNormals = new osg::Vec3Array();
+		myGeometry->setNormalArray(myNormals);
+		myGeometry->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
+	}
+
+	myNormals->push_back(osg::Vec3d(v[0], v[1], v[2]));
+// 	myGeometry->dirtyBound();
+	return myNormals->size() - 1;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void HoudiniGeometry::setNormal(int index, const Vector3f& v)
+{
+	oassert(myNormals->size() > index);
+	osg::Vec3f& c = myNormals->at(index);
+	c[0] = v[0];
+	c[1] = v[1];
+	c[2] = v[2];
+	myNormals->dirty();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+Vector3f HoudiniGeometry::getNormal(int index)
+{
+	oassert(myNormals->size() > index);
+	const osg::Vec3f& c = myNormals->at(index);
+	return Vector3f(c[0], c[1], c[2]);
+}
+
+
 static std::string get_string(int string_handle);
 
 String sOtl_file;
@@ -76,11 +138,19 @@ public:
 	virtual void onMenuItemEvent(MenuItem* mi);
 	virtual void onSelectedChanged(SceneNode* source, bool value);
 
+	void process_assets(const hapi::Asset &asset);
+	void process_geo_part(const hapi::Part &part);
+	void process_float_attrib(
+	    const hapi::Part &part, HAPI_AttributeOwner attrib_owner,
+	    const char *attrib_name, vector<Vector3f>& points);
+
 	void wait_for_cook();
 
 private:
 	SceneManager* mySceneManager;
 	StaticObject* myObject;
+
+	SceneNode* sn;
 
 	// Scene editor. This will be used to manipulate the object.
 	SceneEditorModule* myEditor;
@@ -95,6 +165,8 @@ private:
     hapi::Asset* myAsset;
 	int asset_id;
 
+	HoudiniGeometry* hg;
+	vector<String> geoNames;
 
 };
 
@@ -155,8 +227,6 @@ HelloApplication::HelloApplication():
 
 		myAsset = new hapi::Asset(asset_id); // make this a ref pointer later
 
-// 	    print_asset_info(*myAsset);
-
     }
     catch (hapi::Failure &failure)
     {
@@ -180,6 +250,187 @@ HelloApplication::~HelloApplication() {
 		throw;
     }
 }
+
+void HelloApplication::process_assets(const hapi::Asset &asset)
+{
+    vector<hapi::Object> objects = asset.objects();
+    for (int object_index=0; object_index < int(objects.size()); ++object_index)
+    {
+		vector<hapi::Geo> geos = objects[object_index].geos();
+
+		for (int geo_index=0; geo_index < int(geos.size()); ++geo_index)
+		{
+		    vector<hapi::Part> parts = geos[geo_index].parts();
+
+		    for (int part_index=0; part_index < int(parts.size()); ++part_index)
+			{
+
+				String s = "geo" + part_index;
+
+				hg = HoudiniGeometry::create(s);
+				geoNames.push_back(s);
+				process_geo_part(parts[part_index]);
+				mySceneManager->addModel(hg);
+			}
+		}
+    }
+}
+
+void HelloApplication::process_geo_part(const hapi::Part &part)
+{
+    cout << "object " << part.geo.object.id << ", geo " << part.geo.id
+	 << ", part " << part.id << endl;
+
+    // Print the list of point attribute names.
+    cout << "attributes:" << endl;
+    vector<std::string> attrib_names = part.attribNames(
+	HAPI_ATTROWNER_POINT);
+    for (int attrib_index=0; attrib_index < int(attrib_names.size());
+	    ++attrib_index)
+		cout << "    " << attrib_names[attrib_index] << endl;
+
+    cout << "point positions:" << endl;
+
+	vector<Vector3f> points;
+	vector<Vector3f> normals;
+
+	bool has_normals = false;
+
+	// TODO: better process normals, on HAPI_ATTROWNER_VERTEX, or PRIMITIVE
+
+    process_float_attrib(part, HAPI_ATTROWNER_POINT, "P", points);
+
+    for (int attrib_index=0; attrib_index < int(attrib_names.size());
+	    ++attrib_index) {
+		if (attrib_names[attrib_index] == "N") {
+		    cout << "point normals:" << endl;
+			has_normals = true;
+		    process_float_attrib(part, HAPI_ATTROWNER_POINT, "N", normals);
+		}
+	}
+
+    cout << "Number of faces: " << part.info().faceCount << endl;
+    int * face_counts = new int[ part.info().faceCount ];
+    ENSURE_SUCCESS( HAPI_GetFaceCounts(
+		part.geo.object.asset.id,
+		part.geo.object.id,
+		part.geo.id,
+        part.id,
+		face_counts,
+		0,
+		part.info().faceCount
+	) );
+
+    int * vertex_list = new int[ part.info().vertexCount ];
+    ENSURE_SUCCESS( HAPI_GetVertexList(
+		part.geo.object.asset.id,
+		part.geo.object.id,
+		part.geo.id,
+        part.id,
+		vertex_list, 0, part.info().vertexCount ) );
+    cout << "Vertex Indices into Points array:\n";
+    int curr_index = 0;
+
+	hg->setVertexListSize(pointCount);
+
+	int prev_faceCount = face_counts[0];
+    int prev_faceCountIndex = 0;
+
+	// TODO: get primitive set working for different triangles..
+	// TODO: get normals working from Vertex set.
+
+    for( int ii=0; ii < part.info().faceCount; ii++ )
+    {
+        for( int jj=0; jj < face_counts[ii]; jj++ )
+        {
+
+			hg->setVertex(curr_index,points[vertex_list[ curr_index ]]);
+			if (has_normals) {
+				hg->addNormal(-normals[vertex_list[ curr_index ]]));
+	            cout << "normal :" << curr_index << ", belonging to face: " << ii <<", index: "
+	                << vertex_list[ curr_index ] << " of normals array, being:" <<
+	                hg->getNormal(curr_index) << "\n";
+			}
+			hg->addColor(Color(0.8f, 0.8f, 0.8f, 1.0));
+
+            cout << "vertex :" << curr_index << ", belonging to face: " << ii <<", index: "
+                << vertex_list[ curr_index ] << " of points array, being:" <<
+                points[vertex_list[ curr_index ]] << "\n";
+            curr_index++;
+        }
+
+		if (face_counts[ii] != prev_faceCount) {
+			osg::PrimitiveSet::Mode myType;
+
+			if (face_counts[ii] == 3) {
+				myType = osg::PrimitiveSet::TRIANGLES;
+			} else if (face_counts[ii] == 4) {
+				myType = osg::PrimitiveSet::QUADS;
+			} else if (face_counts[ii] > 4) {
+				cout << "face count is " << face_counts[ii] << endl;
+				myType = osg::PrimitiveSet::TRIANGLE_FAN;
+			}
+
+			cout << "making primitive set for " << face_counts[ii] << ", from " <<
+				prev_faceCountIndex << " to " <<
+				1 + curr_index << endl;
+
+			hg->addPrimitiveOsg(myType, prev_faceCountIndex, 1 + curr_index);
+			prev_faceCountIndex = curr_index + 1;
+			prev_faceCount = face_counts[ii];
+		}
+    }
+
+	osg::PrimitiveSet::Mode myType;
+
+	if (prev_faceCount == 3) {
+		myType = osg::PrimitiveSet::TRIANGLES;
+	} else if (prev_faceCount == 4) {
+		myType = osg::PrimitiveSet::QUADS;
+	} else if (prev_faceCount > 4) {
+		cout << "face count is " << prev_faceCount << endl;
+		myType = osg::PrimitiveSet::TRIANGLE_FAN;
+	}
+
+    hg->addPrimitiveOsg(myType, prev_faceCountIndex, 1 + curr_index);
+
+    delete[] face_counts;
+    delete[] vertex_list;
+
+}
+
+
+void HelloApplication::process_float_attrib(
+    const hapi::Part &part, HAPI_AttributeOwner attrib_owner,
+    const char *attrib_name, vector<Vector3f>& points)
+{
+    // Get the attribute values.
+    HAPI_AttributeInfo attrib_info = part.attribInfo(attrib_owner, attrib_name);
+    float *attrib_data = part.getNewFloatAttribData(attrib_info, attrib_name);
+
+	points.clear();
+	points.resize(attrib_info.count);
+    for (int elem_index=0; elem_index < attrib_info.count; ++elem_index)
+    {
+
+		Vector3f v;
+		for (int tuple_index=0; tuple_index < attrib_info.tupleSize;
+			++tuple_index)
+		{
+
+			v[tuple_index] = attrib_data[elem_index * attrib_info.tupleSize + tuple_index ];
+
+		    cout << attrib_data[
+			    elem_index * attrib_info.tupleSize + tuple_index]
+			<< " ";
+		}
+		cout << endl;
+		points[elem_index] = v;
+    }
+
+    delete [] attrib_data;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void HelloApplication::initialize()
 {
@@ -207,13 +458,13 @@ void HelloApplication::initialize()
 	myEditor->addNode(myObject);
 	myEditor->setEnabled(false);
 
-	// Create a plane for reference.
-	PlaneShape* plane = new PlaneShape(mySceneManager, 100, 100, Vector2f(50, 50));
-	plane->setName("ground");
-	//plane->setEffect("textured -d cyclops/test/checker2.jpg -s 20 -g 1 -v envmap");
-	plane->setEffect("textured -d cubemaps/gradient1/negy.png");
-	plane->pitch(-90 * Math::DegToRad);
-	plane->setPosition(0, 0, -2);
+// 	// Create a plane for reference.
+// 	PlaneShape* plane = new PlaneShape(mySceneManager, 100, 100, Vector2f(50, 50));
+// 	plane->setName("ground");
+// 	//plane->setEffect("textured -d cyclops/test/checker2.jpg -s 20 -g 1 -v envmap");
+// 	plane->setEffect("textured -d cubemaps/gradient1/negy.png");
+// 	plane->pitch(-90 * Math::DegToRad);
+// 	plane->setPosition(0, 0, -2);
 
 	// Setup a light for the scene.
 	Light* light = new Light(mySceneManager);
@@ -241,6 +492,16 @@ void HelloApplication::initialize()
 	// Create the root menu
 	ui::Menu* menu = myMenuManager->createMenu("menu");
 	myMenuManager->setMainMenu(menu);
+
+	sn = SceneNode::create("myOtl");
+
+    process_assets(*myAsset);
+
+	for (int i = 0; i < geoNames.size(); ++i) {
+		StaticObject* hgGeoInstance = new StaticObject(mySceneManager, geoNames[i]);
+// 		sn->addChild(hgGeoInstance);
+	}
+	myEditor->addNode(sn);
 
 	// Add the 'manipulate object' menu item. This item will be a checkbox. We attach this class as
 	// a listener for the menu item: When the item is activated, the menu system will call
