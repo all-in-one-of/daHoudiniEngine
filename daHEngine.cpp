8cd8a08c423bac18d9e336872252bcc2b764d252
luke.cole <luke.cole@uts.edu.au>
2016-12-23 14:42:28 +1100

    Code cleanup

--- daHEngine.cpp
+++ daHEngine.cpp
@@ -74,8 +74,7 @@ BOOST_PYTHON_MODULE(daHEngine)
  		PYAPI_REF_GETTER(HoudiniEngine, getHoudiniCont)
  		PYAPI_REF_GETTER(HoudiniEngine, getStagingCont)
  		PYAPI_REF_GETTER(HoudiniEngine, getHG)
-        PYAPI_METHOD(HoudiniEngine, loadParameters)
-		;
+        PYAPI_METHOD(HoudiniEngine, loadParameters);
 
 	// HoudiniGeometry
 	PYAPI_REF_BASE_CLASS(HoudiniGeometry)
