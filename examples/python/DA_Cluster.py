acc354ef6bc4bec4db4d946580ff7f5ec5180f33
Darren Lee <darren.lee@uts.edu.au>
2018-10-08 14:21:33 +1100

    add SpaceNav controls to examples

--- examples/python/DA_Cluster.py
+++ examples/python/DA_Cluster.py
@@ -2,6 +2,20 @@ from cyclops import *
 from daHEngine import *
 from omegaToolkit import *
 
+# spaceNav control
+import DA_spaceNav
+
+# Change these lines for initial control sensitivities
+rotSensitivity = 0.4
+transSensitivity = 0.1
+# Change these lines for initial position and orientation of the object
+initPos = Vector3(0, 0, 0)
+#initRot = quaternionFromEulerDeg(30, 30, 30)
+initRot = quaternionFromEulerDeg(0, 0, 0)
+rotOffset = quaternionFromEulerDeg(0, 0, 0)
+
+cam = getDefaultCamera()
+
 import os
 # otl path relative to script location
 baseDir = os.path.realpath('../../otl') + "/"
@@ -15,19 +29,17 @@ ui = UiModule.createAndInitialize()
 wf = ui.getWidgetFactory()
 uiroot = ui.getUi()
 
-def createHG(otl, assetName, geoName):
-    global baseDir
-    print "loading", baseDir + otl
-    he.loadAssetLibraryFromFile(baseDir + otl)
-    print "instantiating", assetName
-    he.instantiateAsset(assetName)
-    print "creating geometry", geoName
-    return he.instantiateGeometry(geoName)
-
-# 3d cluster example
-asset = createHG("cluster.otl", "Object/cluster", "cluster1") 
+# 3d Cluster Example (cluster.otl)
+# load Houdini Digital Asset
+he.loadAssetLibraryFromFile(baseDir + "cluster.otl")
+# load particular asset by name
+he.instantiateAsset("Object/cluster")
+# create HoudiniAsset
+asset = he.instantiateGeometry("cluster1") 
 asset.setPosition(-4, -3, -35)
-hg = he.getHG("cluster1")
+
+# create a houdiniGeometry (equivalent of ModelGeometry)
+# hg = he.getHG("cluster1")
 
 # make a light in the scene
 light = Light.create()
@@ -36,33 +48,48 @@ light.setPosition(0, 4, 20)
 print "loaded everything, running.."
 
 # list all the parameters for this asset
-myParms = he.getParameters('cluster1')
-for parm in myParms.keys():
-    print parm, myParms[parm]
+if isMaster():
+    myParms = he.getParameters('cluster1')
+    for parm in myParms.keys():
+        print parm, myParms[parm]
 
-# examples of getting/setting parms
-he.setParameterValue('cluster1', 'filename', "/local/examples/hsdh/HSDH_example.csv")
-he.setParameterValue('cluster1', 'maxrows', 1)
+    # examples of getting/setting parms
+    he.setParameterValue('cluster1', 'filename', "/local/examples/hsdh/HSDH_example.csv")
+    he.setParameterValue('cluster1', 'maxrows', 1)
+    he.setParameterValue('cluster1', 'tShowMetaball', 0)
 
-# set parameters based on menu items
-print he.getParameterChoices('clister1', 'nVal')
-#choice lists can be set by index (value) or by Label
-he.setParameterValue('cluster1', 'nVal', 2) # 'LGA Name'
-he.setParameterValue('cluster1', 'xVal', 9) # 'Total Contracts'
-he.setParameterValue('cluster1', 'yVal', 'Population') # 10
-he.setParameterValue('cluster1', 'zVal', 'SEIFA Index') # 3
-he.setParameterValue('cluster1', 'cVal', 3) # SEIFA
+    # set parameters based on menu items
+    print he.getParameterChoices('clister1', 'nVal')
+    #choice lists can be set by index (value) or by Label
+    he.setParameterValue('cluster1', 'nVal', 2) # 'LGA Name'
+    he.setParameterValue('cluster1', 'xVal', 9) # 'Total Contracts'
+    he.setParameterValue('cluster1', 'yVal', 'Population') # 10
+    he.setParameterValue('cluster1', 'zVal', 'SEIFA Index') # 3
+    he.setParameterValue('cluster1', 'cVal', 3) # SEIFA
 
-# add a parm instance!
-he.insertMultiparmInstance('cluster1', 'colRamp', 2)
+    # add a parm instance!
+    he.insertMultiparmInstance('cluster1', 'colRamp', 2)
 
-# set colours!
-he.setParameterValue('cluster1', 'colRamp1c', [0.8,0.1,0.1]) # Red
-he.setParameterValue('cluster1', 'colRamp2c', [0.1,0.8,0.1]) # Green
-he.setParameterValue('cluster1', 'colRamp2pos', 0.5)
-he.setParameterValue('cluster1', 'colRamp3c', [0.1,0.1,0.8]) # Blue
-he.setParameterValue('cluster1', 'colRamp3pos', 1.0)
-he.setParameterValue('cluster1', 'maxrows', 25)
+    # set colours!
+    he.setParameterValue('cluster1', 'colRamp1c', [0.8,0.1,0.1]) # Red
+    he.setParameterValue('cluster1', 'colRamp2c', [0.1,0.8,0.1]) # Green
+    he.setParameterValue('cluster1', 'colRamp2pos', 0.5)
+    he.setParameterValue('cluster1', 'colRamp3c', [0.1,0.1,0.8]) # Blue
+    he.setParameterValue('cluster1', 'colRamp3pos', 1.0)
+    he.setParameterValue('cluster1', 'maxrows', 25)
 
-# cook all the data (could take some time)
-he.setParameterValue('cluster1', 'usemaxrows', 0)
+    # cook all the data (could take some time)
+    he.setParameterValue('cluster1', 'usemaxrows', 0)
+
+## On run
+if __name__ == "__main__":
+	DA_spaceNav.obj = cam
+	DA_spaceNav.rotSensitivity = rotSensitivity
+	DA_spaceNav.transSensitivity = transSensitivity 
+	DA_spaceNav.initPos = initPos
+	DA_spaceNav.initRot = initRot
+	# added for mLab 28Sep15
+	DA_spaceNav.pivotOffset = Vector3(0,-2,0)
+	DA_spaceNav.rotOffset = rotOffset
+	setEventFunction(DA_spaceNav.onEvent)
+	setUpdateFunction(DA_spaceNav.onUpdate)
--- examples/python/DA_HEngine.py
+++ examples/python/DA_HEngine.py
@@ -2,6 +2,20 @@ from cyclops import *
 from daHEngine import *
 from omegaToolkit import *
 
+# spaceNav control
+import DA_spaceNav
+
+# Change these lines for initial control sensitivities
+rotSensitivity = 0.4
+transSensitivity = 0.1
+# Change these lines for initial position and orientation of the object
+initPos = Vector3(0, 0, 0)
+#initRot = quaternionFromEulerDeg(30, 30, 30)
+initRot = quaternionFromEulerDeg(0, 0, 0)
+rotOffset = quaternionFromEulerDeg(0, 0, 0)
+
+cam = getDefaultCamera()
+
 import os
 # otl path relative to script location
 baseDir = os.path.realpath('../../otl') + "/"
@@ -9,6 +23,9 @@ baseDir = os.path.realpath('../../otl') + "/"
 # init stuff
 he = HoudiniEngine.createAndInitialize()
 he.setLoggingEnabled(False)
+co = CookOptions()
+co.splitGeosByGroup = True
+he.setCookOptions(co)
 hg = None
 
 ui = UiModule.createAndInitialize()
@@ -39,6 +56,8 @@ examples = [
     ("curve_object.otl", "Object/curve_object", "curve_object1"),
     # cluster otl example
     ("cluster.otl", "Object/cluster", "cluster1"),
+    # materials test
+    ("mat_test.otl", "Object/mat_test", "mat_test1"),
     # multiple assets in library
     ("axisA1.otl", "Object/axis3D", "axis3D1")
 ]
@@ -76,6 +95,12 @@ asset = createHG(*examples[-1]) # 3d graph example
 asset.setPosition(-1, 2, -5)
 hg = he.getHG("axis3D1")
 
+# asset = createHG(*examples[-1]) # materials
+# asset.setPosition(0, 2, -5)
+# hg = he.getHG("mat_test1")
+# he.setParameterValue('mat_test1', 'keepgroups', 1)
+# he.setParameterValue('mat_test1', 'multimat', 1)
+
 # asset = createHG(*examples[-2]) # cluster example
 # asset.setPosition(-1, 2, -5)
 # hg = he.getHG("cluster1")
@@ -88,13 +113,8 @@ hg = he.getHG("axis3D1")
 
 #asset = createHG(*examples[9]) # curve
 
-#sp = SphereShape.create(.1, 1)
-#sp.setPosition(0,2,-5)
-
 light = Light.create()
 light.setPosition(0, 4, 0)
-
-e = None
 
 # my houdini geometry counts
 if hg != None:
@@ -110,3 +130,16 @@ def onEvent():
 
 print "loaded everything, running.."
 setEventFunction(onEvent)
+
+## On run
+if __name__ == "__main__":
+	DA_spaceNav.obj = cam
+	DA_spaceNav.rotSensitivity = rotSensitivity
+	DA_spaceNav.transSensitivity = transSensitivity 
+	DA_spaceNav.initPos = initPos
+	DA_spaceNav.initRot = initRot
+	# added for mLab 28Sep15
+	DA_spaceNav.pivotOffset = Vector3(0,-2,0)
+	DA_spaceNav.rotOffset = rotOffset
+	setEventFunction(DA_spaceNav.onEvent)
+	setUpdateFunction(DA_spaceNav.onUpdate)
